e14222db07faf210c5d455b52def700b59b7781e
Adrian Aro <aroadrianchristopher@gmail.com>
2025-02-12 19:08:47 +0800

    successfully run crud for read_person

--- python_crud.py
+++ python_crud.py
@@ -7,7 +7,7 @@
 import psycopg2
 
 
-def create_person(conn, firstname, lastname, age):
+def create_person(conn, firstname, lastname, age, ):
     cursor = conn.cursor()
     cursor.execute("INSERT INTO person (firstname, lastname, age) VALUES (%s, %s, %s)", (firstname, lastname, age))
     conn.commit()
@@ -21,7 +21,11 @@ def read_all_persons(conn):
     return persons
 
 def read_person(conn, person_id):
-    pass
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM person WHERE id = %s", (person_id,))
+    one_person = cursor.fetchone()
+    cursor.close()
+    return one_person
 
 def update_person(conn, person_id, person):
     pass
@@ -34,12 +38,16 @@ if __name__ == "__main__":
     conn = psycopg2.connect(
         host="localhost",
         database="postgres",
-        user="admin",
+        user="postgres",
         password="admin"
     )
 
-    # create_person(conn, "John", "Doe", 30)
+    #create_person(conn, "Adrian", "Aro", 25)
     persons = read_all_persons(conn)
     print(persons)
+    person_id = 3
+    one_person = read_person(conn, person_id)
+    print(one_person)
 
-    print(persons[0][1])
+
+    #print(persons[0][1])
